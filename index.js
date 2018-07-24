f17bffa27d3267fff1744db36cf166c46c3b2aad
Noelle Rolwes <nrolwes@gmail.com>
2018-07-24 21:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function kittens(){
 }
 
 function destructivelyAppendKitten(name){
-  destructivelyAppendKitten.push('ralph');
+  return arrays.push('ralph');
 }
 
 function destructivelyPrependKitten(name){
